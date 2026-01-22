1f8720973bc4f4ac3ead176603c81cedb5f46c42
Roger Palmada <roger.palmada@gmail.com>
2026-01-22 20:07:39 +0100

    Main1 executa totes les URLS, main2 utilitza beautifulSoup

--- main2.py
+++ main2.py
@@ -1,4 +1,6 @@
-from newspaper import Article
+import requests
+from bs4 import BeautifulSoup
+
 TITLES = [
     "ARTICLE MARC",
     "LAMINE YAMAL",
@@ -10,15 +12,29 @@ url1 = "https://link.springer.com/article/10.1186/s12915-020-00925-x"
 url2 = "https://en.nogomania.com/read/Was-it-really-offside-Barca-rage-after-controversial-decision-in-San-Sebastian#goog_rewarded"
 url3 = "https://goldbroker.com/news/fallacy-inverse-relationship-between-gold-dollar-index-dxy-3363"
 url4 = "https://www.nbcnews.com/id/wbna3541441"
+# url5 crashes amb BeautifulSoup
 url5 = "https://www.ap.org/the-definitive-source/behind-the-news/spain-train-crash-how-a-journalists-quick-thinking-led-to-vital-info/"
 URLS = [url1, url2, url3, url4, url5]
 
-print("".ljust(80, "-"))
-for title, url in zip(TITLES, URLS):
-    print(title)
-    print("\n")
-    art = Article(url)
-    art.download()
-    art.parse()
-    print(art.text)
-    print("".ljust(80, "-"))
+response = requests.get(url5)
+html = response.text
+soup = BeautifulSoup(html, 'html.parser')
+
+article = soup.find("article")
+
+if article:
+    text = article.get_text(separator=" ", strip=True)
+else:
+    text = soup.get_text(separator=" ", strip=True)
+
+
+# PRINT TITLE
+h1 = soup.find("h1")
+if h1:
+    title = h1.get_text(strip=True)
+else:
+    title = soup.find("title").get_text(strip=True)
+
+
+print("TITLE:", title)
+print(text)
