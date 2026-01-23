aebb9b7e10cd291cf547ac8cedef957ae9288051
Alex Rodriguez <21alexrodri@gmail.com>
2026-01-23 20:06:25 +0100

    class Article Extractor

--- model/article_class.py
+++ model/article_class.py
@@ -3,4 +3,43 @@ from bs4 import BeautifulSoup
 from newspaper import Article
 
 class ArticleExtractor:
+    def __init__(self, url, title = None):
+        self.url = url
+        if title is None:
+            self.title = self.extract_title()
+        else:
+            self.title = title
+        
     
+    def extract_text_newspaper_method(self):
+        art = Article(self.url)
+        art.download()
+        art.parse()
+        return art.text
+
+    def extract_text_bs4_method(self):
+        response = requests.get(self.url)
+        html = response.text
+        soup = BeautifulSoup(html, 'html.parser')
+
+        article = soup.find("article")
+
+        if article:
+            text = article.get_text(separator=" ", strip=True)
+        else:
+            text = soup.get_text(separator=" ", strip=True)
+        
+        return text
+    
+    def extract_title(self):
+        text = self.extract_text_bs4_method()
+        response = requests.get(self.url)
+        html = response.text
+        soup = BeautifulSoup(html, 'html.parser')
+        h1 = soup.find("h1")
+        if h1:
+            title = h1.get_text(strip=True)
+        else:
+            title = soup.find("title").get_text(strip=True)
+        
+        return title
