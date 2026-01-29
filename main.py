837f4f84bf3519fd7cf71a506c47b57a0fac0a5a
Alex Rodriguez <21alexrodri@gmail.com>
2026-01-29 19:20:05 +0100

    resguardo

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from model.nltk_tokenize_class import NLTKTokenizer
 from model.spacy_tokenize_class import SpacyTokenizer
 from model.article_extractor_class import ArticleExtractor
 from model.article_summarizer_class import ArticleSummarizer
+from model.topic_classifier_class import TopicClassifier
 url = "https://en.nogomania.com/read/Was-it-really-offside-Barca-rage-after-controversial-decision-in-San-Sebastian#goog_rewarded"
 
 # science_article = ArticleExtractor(url)
@@ -41,9 +42,57 @@ article_urls = {
 
 if choice in article_urls:
     url = article_urls[choice]
+
+    print("______________________________\n")
+    print("✅✅✅ Url accepted. ✅✅✅")
+    print("______________________________\n")
+
+
+    # Article text extraction
+    print("Extracting text step...")
+    print("Select the extraction method:")
+    print("1 - Newspaper method (default)")
+    print("2 - BeautifulSoup method")
+    extraction_choice = input("Enter the number of your choice (1-2): ")
+    extractor = ArticleExtractor(url)
+    if extraction_choice == "1":
+        text = extractor.extract_text_newspaper_method()
+    elif extraction_choice == "2":
+        text = extractor.extract_text_bs4_method()
+    else:
+        print("❌❌❌ Invalid choice. Using Newspaper method by default. ❌❌❌")
+        text = extractor.extract_text_newspaper_method()
+    print("✅✅✅ Text extracted successfully. ✅✅✅")
+
+    # Tokenization
+    print("\n______________________________\n")
+    print("Tokenization step...")
+    print("Select the tokenizer:")
+    print("1 - NLTK Tokenizer (default)")
+    print("2 - SpaCy Tokenizer")
+    tokenizer_choice = input("Enter the number of your choice (1-2): ")
+    if tokenizer_choice == "1":
+        tokenizer = NLTKTokenizer()
+    elif tokenizer_choice == "2":
+        tokenizer = SpacyTokenizer()
+    else:
+        print("❌❌❌ Invalid choice. Using NLTK Tokenizer by default. ❌❌❌")
+        tokenizer = NLTKTokenizer()
+
+    tokens = tokenizer.clean_tokenize(text)
+    print(f"Number of tokens: {len(tokens)}")
+    print("✅✅✅ Tokenization completed successfully. ✅✅✅")
+
+    # Thematic Classification
+    print("\n______________________________\n")
+    print("Thematic Classification step...")
+    classifier = TopicClassifier()
+    
+
+
     
 else:
-    print("Invalid choice. Exiting the program.")
+    print("❌❌❌ Invalid choice. Exiting the program. ❌❌❌")
 
 
 
